e5407e769a4f3e865c66cdc6771b81bf00f314ed
Benjamin Durbin <bwdurbin@gmail.com>
2017-08-26 23:11:20 +0000

    Assignment 7: Complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,8 +2,15 @@
    /*global buzz*/
  (function() {
      function SongPlayer() {
+         /**
+         * @desc Service returns this object, making its properties and methods public to the rest of the application
+         * @type {Object}
+         */         
         var SongPlayer = {};
-        
+         /**
+         * @desc Stores currently playing song from AlbumCtrl.albumData.songs{}
+         * @type {Object}
+         */        
         var currentSong = null;
          /**
          * @desc Buzz object audio file
@@ -28,19 +35,35 @@
  
             currentSong = song;
         };
-        
+         /**
+         * @function playSong
+         * @desc Plays current song{Object} stored in currentBuzzObject. Changes playing boolean to true so html icon updates properly.
+         * @param {Object} song
+        */        
+        var playSong = function(song){
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+         /**
+         * @function SongPlayer.play
+         * @desc Checks if currentSong matches passed @param. If false it will set a new song and play. If true, it will resume playing @param
+         * @param {Object} song
+        */        
         SongPlayer.play = function(song){
             if(currentSong !== song){
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song){
                 if(currentBuzzObject.isPaused()){
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
-        
+         /**
+         * @function SongPlayer.pause
+         * @desc Pauses currently playing song and sets bool to false. 
+         * @param {Object} song
+        */   
         SongPlayer.pause = function(song){
             currentBuzzObject.pause();
             song.playing = false;
